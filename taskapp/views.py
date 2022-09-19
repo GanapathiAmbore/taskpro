5a0449f823a15256905fc64608f07b92fbef0779
GanapathiAmbore <ganapathiambore@gmail.com>
2022-09-19 22:35:47 +0530

    added api

--- taskapp/views.py
+++ taskapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from taskapp.models import Post
+from taskapp.serializers import PostSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 def home(request):
     posts = Post.objects.all()
     return render(request,'home.html',{'posts':posts})
+@api_view(['GET'])
+def post_api(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts,many=True)
+    return Response(serializer.data)
